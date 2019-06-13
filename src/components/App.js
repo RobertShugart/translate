5de5fb09206291c4500432bf9534f2d9e94d2793
RobertS <rshugart@protonmail.com>
2019-06-13 17:28:05 -0400

    Added icons and onClick handler

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
 class App extends React.Component {
+	state = { language: 'english' };
+
 	render() {
-		return <div>App</div>;
+		return (
+			<div className="ui container">
+				<div>
+					Select a language:
+					<i className="flag us" onClick={() => this.onLanguageChange('english')} />
+					<i className="flag nl" />
+				</div>
+			</div>
+		);
 	}
 }
 
